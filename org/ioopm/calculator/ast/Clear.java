88671e85ecbdf95da6c731b262782613bfaf1144
lshoravi <linus.shoravi.5307@student.uu.se>
2018-12-10 13:23:27 +0100

    Documentation for clear

--- org/ioopm/calculator/ast/Clear.java
+++ org/ioopm/calculator/ast/Clear.java
@@ -1,10 +1,16 @@
 package org.ioopm.calculator.ast;
 
+/** The clear class is a singleton that represents a
+ * command that removes any variable bindings in the environment.
+ * @author Nils & Astrid
+ * @version 1.0
+ */
 public class Clear extends Command {
     private static final Clear theInstance = new Clear();
 
     private Clear() {}
 
+    /** Returns the singleton. */
     public static Clear instance() {
         return theInstance;
     }
